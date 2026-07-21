d5f6f1585a48f53ddb056a9535dbf5ccc290e42e
ahmad-DS <ahmad.shakil444@gmail.com>
2026-07-21 22:55:13 +0530

    build fix

--- backend/dist/Routes/todos.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import TodoController from "../controllers/todo.controller.js";
-const router = Router();
-router.get("/", TodoController.getUserTodos);
-router.post("/create", TodoController.createUserTodo);
-router.patch("/:todoId", TodoController.updateTodoById);
-router.delete("/:todoId", TodoController.deleteTodoById);
-export default router;
--- backend/dist/container/container.js
+++
@@ -1,5 +0,0 @@
-import TodoService from "../services/todo.service.js";
-import { MongoRepository } from "../repositories/mongoRepository.js";
-const todoRepository = new MongoRepository();
-const todoService = new TodoService(todoRepository);
-export { todoService };
